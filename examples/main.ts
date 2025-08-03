1680e04599746a4155a74f31ce6749f1afcabcfd
JUN-1999 <https://github.com/JUN-1999>
2025-08-03 09:34:57 +0800

    1

--- examples/main.ts
+++ examples/main.ts
@@ -8,7 +8,7 @@ import "element-plus/dist/index.css";
 // 本地开发
 
 // 打包之后测试
-// import "../dist/style.css";
+// import "../dist/style.css";  
 
 // 测试发布的包样式
 // import '@jun1999/components/dist/style.css';
